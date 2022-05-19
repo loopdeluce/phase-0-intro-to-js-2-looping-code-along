b0d905a9f0be187bc3337003b465c5abf580f2fd
Lucy <lucy.d.bullen@gmail.com>
2022-05-18 22:31:16 -0600

    commit tree 75615ac9cf2019df51a159f2fb98840af7cc72f7

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(names, event) {
+  const messages = [];
+  for(let counter = 0; counter < names.length; counter++) {
+    messages.push(`Thank you, ${names[counter]}, for the wonderful ${event} gift!`)
+  }
+  return messages;
+}
+
+function countDown(start) {
+  while(start >= 0) {
+    console.log(start);
+    start--;
+  }
+}
